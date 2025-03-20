7c23ee050b6a92f6e8862fecd92d2d9475db9b1e
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-20 19:05:20 +0500

    Fetching data for favrite places

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,32 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isLoadingData = signal<boolean>(false);
+  error = signal('');
+  private destroyRef = inject(DestroyRef);
+  private httpClient = inject(HttpClient);
+  // Sending http get request
+  ngOnInit(): void {
+    this.isLoadingData.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(map((respData) => respData.places))
+      .subscribe({
+        next: (places) => {
+          console.log(places);
+          this.places.set(places);
+        },
+        error: (error) => {
+          this.error.set('Some thing wents wrong while fetching data....');
+        },
+        complete: () => {
+          this.isLoadingData.set(false);
+        },
+      });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
